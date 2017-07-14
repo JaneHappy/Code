353d66a42d006f76f4643ff9fba5bdf3c9ae20d7
JaneHappy <1584360631@qq.com>
2017-07-14 20:39:55 +0800

    word2vec

---
+++ Google_DL_framework/chap-3_basic.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Google DeepLearning framework
+# chapter 3
+
+
+
+
+#=============================
+#  chapter 3.1    Graph
+#=============================
+
+import tensorflow as tf 
+
+a = tf.constant([1., 2.], name="a")
+b = tf.constant([2., 3.], name="b")
+result = a + b
+
+
+
+
+
+
