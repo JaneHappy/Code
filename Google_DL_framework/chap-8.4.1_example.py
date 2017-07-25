839e4cc32528f09a90a55575874480d934dbc5c6
JaneHappy <1584360631@qq.com>
2017-07-25 11:48:13 +0800

    chap 8.4.1 (didn't work)

--- Google_DL_framework/chap-8.4.1_example.py
+++ Google_DL_framework/chap-8.4.1_example.py
@@ -12,27 +12,44 @@ import tensorflow as tf
 # from tensorflow.models.rnn.ptb import reader
 import reader
 
+
 # DATA_PATH = "/path/to/ptb/data" 	# 数据存放的路径
 DATA_PATH = "/home/ubuntu/Program/GoogleDL_datasets/simple-examples/data"
 HIDDEN_SIZE = 200 					# 隐藏层规模
-# 深层循环神经网络中LSTM结构的层数。
-# 词典规模，加上语句结束标识符和稀有单词标识符总共一万个单词。
-# 学习速率。
-# 训练数据batch的大小。
-# 训练数据截断长度。
+NUM_LAYERS = 2 						# 深层循环神经网络中LSTM结构的层数。
+VOCAB_SIZE = 10000 					# 词典规模，加上语句结束标识符和稀有单词标识符总共一万个单词。
+
+LEARNING_RATE = 1.0 				# 学习速率。
+TRAIN_BATCH_SIZE = 20 				# 训练数据batch的大小。
+TRAIN_NUM_STEP = 35 				# 训练数据截断长度。
+
 
 # 在测试时不需要使用截断，所以可以将测试数据看成一个超长的序列。
-# 测试数据batch的大小。
-# 测试数据截断长度。
-# 使用训练数据的轮数。
-# 节点不被dropout的概率。
-# 用于控制梯度膨胀的参数。
+EVAL_BATCH_SIZE = 1 		# 测试数据batch的大小。
+EVAL_NUM_STEP = 1 			# 测试数据截断长度。
+NUM_EPOCH = 2 				# 使用训练数据的轮数。
+KEEP_PROB = 0.5 			# 节点不被dropout的概率。
+MAX_GRAD_NORM = 5 			# 用于控制梯度膨胀的参数。
+
 
 # 通过一个 PTBModel 类来描述模型，这样方便维护循环神经网络中的状态。
-# 记录使用的batch大小和截断长度。
-# 定义输入层。可以看到输入层的维度为 batch_size x num_steps，这和 ptb_iterator 函数输出的训练数据 batch 是一致的。
-# 定义预期输出。它的维度和 ptb_iterator 函数输出的正确答案维度也是一样的。
-# 定义使用LSTM结构为循环体结构且使用 dropout 的深层循环神经网络。
+class PTBModel(object):
+	"""docstring for PTBModel"""
+	def __init__(self, is_training, batch_size, num_steps):
+		# 记录使用的batch大小和截断长度。
+		self.batch_size = batch_size
+		self.num_steps = num_steps
+
+		# 定义输入层。可以看到输入层的维度为 batch_size x num_steps，这和 ptb_iterator 函数输出的训练数据 batch 是一致的。
+		self.input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+
+		# 定义预期输出。它的维度和 ptb_iterator 函数输出的正确答案维度也是一样的。
+		self.targets = tf.placeholder(tf.int32, [batch_size, num_steps])
+
+		# 定义使用LSTM结构为循环体结构且使用 dropout 的深层循环神经网络。
+		lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_SIZE)
+		if is_training :
+			lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_)
 # 初始化最初的状态，也就是全零的向量。
 # 将单词ID转换成为单词向量。因为总共有 VOCAB_SIZE 个单词，每个单词向量的维度为 HIDDEN_SIZE，所以 embedding 参数的维度为 VOCAB_SIZE x HIDDEN_SIZE 。
 # 将原本 batch_size x num_steps 个单词ID转化为单词向量，转化后的输入层维度为 batch_size x num_steps x HIDDEN_SIZE。
