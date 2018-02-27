a76f3d649e191c9e4728d6b59b2e66ced65a8979
JaneHappy <1584360631@qq.com>
2018-02-27 17:52:06 +0800

    init

---
+++ tensorflow_cookbook/ch1-2_tensors.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+# tensorflow_cookbook
+#	01_Introduction
+#		01_How_TensorFlow_Works
+#		02_Creating_and_Using_Tensors
+#		03_Using_Variables_and_Placeholders
+
+
+
+
+import tensorflow as tf 
+
+
+
+
+#======================
+# 01 Introduction
+#======================
+
+#----------------------
+# 01 Creating and Using Tensors
+#----------------------
+
+# First we start with loading TensorFlow and reseting the computational graph.
+from tensorflow.python.framework import ops
+ops.reset_default_graph()
+
+# Start a graph session
+sess = tf.Session()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
