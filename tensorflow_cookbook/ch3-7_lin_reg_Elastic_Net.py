14ff39854d1ed13d23e8203fad19d88e2596728c
JaneHappy <1584360631@qq.com>
2018-03-01 22:38:21 +0800

    chap 3.7 (not finished)

---
+++ tensorflow_cookbook/ch3-7_lin_reg_Elastic_Net.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# https://github.com/nfmcclure/tensorflow_cookbook/tree/master/03_Linear_Regression/07_Implementing_Elasticnet_Regression
+
+from __future__ import division
+from __future__ import print_function
+
+#	 import required libraries
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+
+
+
+
