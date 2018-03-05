3a3b7f555b3ef57b6abfe356e7d73050ea1c17ed
JaneHappy <1584360631@qq.com>
2018-03-05 11:10:34 +0800

    README: TensorFlow (updated)

---
+++ tensorflow_cookbook/ch8-4_retraining.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# https://github.com/nfmcclure/tensorflow_cookbook/blob/master/08_Convolutional_Neural_Networks/04_Retraining_Current_Architectures/04_download_cifar10.py#L4
+
+from __future__ import division
+from __future__ import print_function
+
+
+
+# Download/Saving CIFAR-10 images in Inception format
+#---------------------------------------
+#
+# In this script, we download the CIFAR-10 images and
+# transform/save them in the Inception Retrianing Format
+#
+# The end purpose of the files is for retrianing the
+# Google Inception tensorflow model to work on the CIFAR-10.
+
+
+
+
+
