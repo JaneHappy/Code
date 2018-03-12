c92d05d72faf26f726a5d15eef19270d1191adbc
JaneHappy <1584360631@qq.com>
2018-03-12 21:03:38 +0800

    init

--- pytorch_tutorial/abc-1_basic.py
+++ pytorch_tutorial/abc-1_basic.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 
 
 
+'''
 class Net(nn.Module):
 	"""docstring for Net"""
 	def __init__(self):
@@ -23,6 +24,24 @@ class Net(nn.Module):
 		self.fc1 = nn.Linear(16*5*5, 120)  # an affine operation: y = Wx+b
 		self.fc2 = nn.Linear(120, 84)
 		self.fc3 = nn.Linear(84, 10)
+'''
+
+
+
+
+# 基本的网络构建类模板
+class net_name(nn.Module):
+	"""docstring for net_name"""
+	def __init__(self):
+		super(net_name, self).__init__()
+		# 可以添加各种网络层
+		self.conv1 = nn.Conv2d(3, 10, 3)  # 1 input image channel, 10 output channels, 3x3 square convolution kernel
+		# 具体每种层的参数可以去查看文档
+
+	def forward(self, x):
+		# 定义向前传播
+		out = self.conv1(x)
+		return out
 
 
 
@@ -36,4 +55,5 @@ class Net(nn.Module):
 '''
 References:
 https://www.jianshu.com/p/a9571c537b59
+http://www.pytorchtutorial.com/10-minute-pytorch-0/
 '''
