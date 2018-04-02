b1d80dca52edf7880bb88617ed969683c1f85797
JaneHappy <1584360631@qq.com>
2018-04-02 12:22:35 +0800

    RNN LSTM Shakespeare.txt

--- tensorflow_cookbook/ch9-3_rnn_lstm.py
+++ tensorflow_cookbook/ch9-3_rnn_lstm.py
@@ -44,7 +44,7 @@ prime_texts 		:	List of test sentences
 min_word_fred 	 = 5 		# Trim the less frequent words off
 rnn_size 		 = 128 		# RNN Model size
 embedding_size 	 = 100 		# Word embedding size
-epochs 			 = 10 		# Number of epochs to cycle through data
+epochs 			 = 2 #10 		# Number of epochs to cycle through data
 batch_size 		 = 100 		# Train on this many examples at once
 learning_rate 	 = 0.001 	# Learning rate
 training_seq_len = 50 		# How long of a word group to consider
@@ -183,7 +183,7 @@ for ix, x in enumerate(s_text_words):
 	try:
 		s_text_ix.append( vocab2ix[x] )
 	except:
-		s_text_ix.append( 0 )
+		s_text_ix.append( 0 ) 	#s_text_ix.append( vocab2ix['unknown'] )
 s_text_ix = np.array(s_text_ix)
 
 '''
@@ -310,6 +310,14 @@ class LSTM_Model():
 			x = np.zeros((1, 1))
 			x[0, 0] = vocab[word]
 			feed_dict = {self.x_data: x, self.initial_state: state}
+			[state] = sess.run([self.final_state], feed_dict=feed_dict)
+
+		out_sentence = prime_text
+		word = word_list[-1]
+		for n in range(num):
+			x = np.zeros((1, 1))
+			x[0, 0] = vocab[word]
+			feed_dict = {self.x_data: x, self.initial_state: state}
 			[model_output, state] = sess.run([self.model_output, self.final_state], feed_dict=feed_dict)
 			sample = np.argmax(model_output[0])
 			if sample == 0:
@@ -323,6 +331,15 @@ class LSTM_Model():
 
 # Define LSTM Model
 lstm_model = LSTM_Model(embedding_size, rnn_size, batch_size, learning_rate, training_seq_len, vocab_size)
+'''
+embedding_size 	 = 100 
+rnn_size 		 = 128 
+embedding_size 	 = rnn_size
+batch_size 		 = 100 
+learning_rate 	 = 0.001 
+training_seq_len = 50 
+vocab_size 		 = len(ix2vocab) + 1 	= 9117
+'''
 
 # Tell TensorFlow we are reusing the scope for the testing
 with tf.variable_scope(tf.get_variable_scope(), reuse=True):
@@ -334,9 +351,200 @@ with tf.variable_scope(tf.get_variable_scope(), reuse=True):
 # Create model saver
 saver = tf.train.Saver(tf.global_variables())
 
+## Let's calculate how many batches are needed for each epoch and split up the data accordingly.
+
+# Create batches for each epoch
+num_batches = int(len(s_text_ix) / (batch_size * training_seq_len)) + 1
+# Split up text indices into subarrays, of equal size
+batches = np.array_split(s_text_ix, num_batches)
+# Reshape each split into [batch_size, training_seq_len]
+batches = [np.resize(x, [batch_size, training_seq_len]) for x in batches]
+
+'''
+>>> batch_size = 100
+>>> training_seq_len = 50
+>>> num_batches = int(len(s_text_ix) / (float(batch_size) * training_seq_len)) + 1
+>>> num_batches
+181
+>>> s_text_ix.shape
+(901089,)
+>>> batches = np.array_splits(s_text_ix, num_batches)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'module' object has no attribute 'array_splits'
+>>> batches = np.array_split(s_text_ix, num_batches)
+>>> batches2 = [np.resize(x, [batch_size, training_seq_len])  for x in batches]
+
+>>> batches[0]
+array([6497, 4191, 7136, ..., 3019, 4868, 6969])
+>>> batches2[0]
+array([[6497, 4191, 7136, ..., 6965, 3027, 3667],
+       [1379, 2675,  484, ..., 4492, 2684, 2338],
+       [8376, 1621, 2684, ..., 5200, 8014, 2684],
+       ..., 
+       [1455, 1451, 8014, ..., 1074, 2675,  369],
+       [   0, 8706, 5254, ..., 6949, 1455, 5299],
+       [ 971, 1455, 2405, ..., 1621,  581,    0]])
+>>> batches[0].shape
+(4979,)
+>>> batches2[0].shape
+(100, 50)
+>>> 
+
+>>> for it,t in enumerate(batches):
+...     if it%10 ==9:
+...             print t.shape
+...     else:
+...             print t.shape ,
+...
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+(4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,) (4979,)
+
+(4979,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,) (4978,)
+(4978,)
+
+'''
+
+
+## Initialize all the variables
+
+# Initialize all variables
+init = tf.global_variables_initializer()
+sess.run(init)
+
+
+## Train the model!
+
+# Train model
+train_loss = []
+iteration_count = 1
+for epoch in range(epochs):
+	# Shuffle word indices
+	random.shuffle(batches)
+	# Create targets from shuffled batches
+	targets = [np.roll(x, -1, axis=1)  for x in batches]
+	'''
+	#一个单词是一个样本
+	
+	s_text_ix 	是 s_text 中的这么多个单词、分别对应的索引，即 vocab2ix[单词]
+	batches = np.array_split(s_text_ix, ..) 
+		把 s_text_ix 划分成了 num_batches=181 个小批次，
+		每一批次有接近 batch_size * training_seq_len =5000 个单词，
+		这 5000 个单词可以看成是 batch_size=100 个样本即句子，
+		每个样本/句子中包含 training_seq_len=50 个单词 —— 实际这里是 vocab2ix[单词]
+	batches = [np.resize(x, [batch_size, training_seq_len])  for x in batches]
+		一共有 num_batches=181 个批次。
+		在每一个批次中，把这一批次的接近 5000 个样本，重新组织，
+		变成 100 个样本/句子，每个句子包含 50 个单词 —— 实际这里是 vocab2ix[单词]
+	
+	epochs  = 10
+	batches = a list, with ``num_batches=181'' items, each ``[batch_size, training_seq_len''
+	iteration_count = 1~~(epoches*num_batches = 10*181=1810)
+	'''
+
+	# Run a through one epoch
+	print('Starting Epoch #{} of {}.'.format(epoch+1, epochs))
+	# Reset initial LSTM state every epoch
+	state = sess.run(lstm_model.initial_state)
+	for ix, batch in enumerate(batches):
+		training_dict = {lstm_model.x_data: batch, lstm_model.y_output: targets[ix]}
+		c, h = lstm_model.initial_state
+		training_dict[c] = state.c 
+		training_dict[h] = state.h 
+
+		temp_loss, state, _ = sess.run([lstm_model.cost, lstm_model.final_state, lstm_model.train_op], feed_dict=training_dict)
+		train_loss.append(temp_loss)
+
+		# Print status every 10 gens
+		if iteration_count % 10 == 0:
+			#summary_nums = (iteration_count, epoch+1, ix+1, num_batches+1, temp_loss)
+			#print('Iterations: {}, Epoch: {}, Batch: {} out of {}, Loss: {:.2f}'.format(*summary_nums))
+			summary_nums = (iteration_count, epoch+1, ix+1, num_batches, temp_loss)
+			print('\tIterations: {}, Epoch: {}, Batch: {} out of {}, Loss: {:.6f}'.format(*summary_nums))
+
+		# Save the model and the vocab
+		if iteration_count % save_every == 0:
+			# Save model
+			model_file_name = os.path.join(full_model_dir, 'model')
+			saver.save(sess, model_file_name, globel_step = iteration_count)
+			#print('Model Saved To: {}'.format(model_file_name))
+			print('\t\tModel Saved To: {} .'.format(model_file_name))
+			# Save vocabulary
+			dictionary_file = os.path.join(full_model_dir, 'vocab.pkl')
+			with open(dictionary_file, 'wb') as dict_file_conn:
+				pickle.dump([vocab2ix, ix2vocab], dict_file_conn)
+
+		if iteration_count % eval_every == 0:
+			for sample in prime_texts:
+				print('\t\t\tPredict:\t', end="")
+				print(test_lstm_model.sample(sess, ix2vocab, vocab2ix, num=10, prime_text=sample))
+
+		iteration_count += 1
 
 
 
+
+'''
+2018-04-02 12:06:42.238047: I tensorflow/core/platform/cpu_feature_guard.cc:137] Your CPU supports instructions that this TensorFlow binary was not compiled to use: SSE4.1 SSE4.2 AVX
+Loading Shakespeare Data
+Cleaning Text
+Done loading/cleaning.
+Building Shakespeare Vocab
+Vocabulary Length = 9117
+Starting Epoch #1 of 2.
+	Iterations: 10, Epoch: 1, Batch: 10 out of 181, Loss: 10.007343
+	Iterations: 20, Epoch: 1, Batch: 20 out of 181, Loss: 9.179014
+	Iterations: 30, Epoch: 1, Batch: 30 out of 181, Loss: 8.776321
+	Iterations: 40, Epoch: 1, Batch: 40 out of 181, Loss: 8.622705
+	Iterations: 50, Epoch: 1, Batch: 50 out of 181, Loss: 8.414643
+			Predict:	thou art more hand- like leaden entertain'd made when the
+			Predict:	to be or not to the
+			Predict:	wherefore art thou Sex Muse and
+	Iterations: 60, Epoch: 1, Batch: 60 out of 181, Loss: 8.034399
+	Iterations: 70, Epoch: 1, Batch: 70 out of 181, Loss: 7.787870
+	Iterations: 80, Epoch: 1, Batch: 80 out of 181, Loss: 7.644072
+	Iterations: 90, Epoch: 1, Batch: 90 out of 181, Loss: 7.622431
+	Iterations: 100, Epoch: 1, Batch: 100 out of 181, Loss: 7.494274
+			Predict:	thou art more than often
+			Predict:	to be or not to the
+			Predict:	wherefore art thou At going my lord I am
+	Iterations: 110, Epoch: 1, Batch: 110 out of 181, Loss: 7.717870
+	Iterations: 120, Epoch: 1, Batch: 120 out of 181, Loss: 7.654597
+	Iterations: 130, Epoch: 1, Batch: 130 out of 181, Loss: 7.711281
+	Iterations: 140, Epoch: 1, Batch: 140 out of 181, Loss: 7.740433
+	Iterations: 150, Epoch: 1, Batch: 150 out of 181, Loss: 7.305348
+			Predict:	thou art more than the
+			Predict:	to be or not to the
+			Predict:	wherefore art thou art too cause to the
+	Iterations: 160, Epoch: 1, Batch: 160 out of 181, Loss: 6.868250
+	Iterations: 170, Epoch: 1, Batch: 170 out of 181, Loss: 6.693734
+	Iterations: 180, Epoch: 1, Batch: 180 out of 181, Loss: 6.817922
+Starting Epoch #2 of 2.
+	Iterations: 190, Epoch: 2, Batch: 9 out of 181, Loss: 6.632661
+	Iterations: 200, Epoch: 2, Batch: 19 out of 181, Loss: 6.494490
+			Predict:	thou art more than the
+			Predict:	to be or not to the
+			Predict:	wherefore art thou art part no no more than the
+	Iterations: 210, Epoch: 2, Batch: 29 out of 181, Loss: 6.605542
+	Iterations: 220, Epoch: 2, Batch: 39 out of 181, Loss: 6.852113
+	Iterations: 230, Epoch: 2, Batch: 49 out of 181, Loss: 6.654508
+[Cancelled]
+'''
 
 
 
